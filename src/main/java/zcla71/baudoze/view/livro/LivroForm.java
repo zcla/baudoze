991d399fdfcef5e7aa3fe2992ab3f078b2fbbd22
zcla <zeclaudioaguiar@gmail.com>
2024-07-19 02:54:29 +0000

    Collection -> List [2]

--- src/main/java/zcla71/baudoze/view/livro/LivroForm.java
+++ src/main/java/zcla71/baudoze/view/livro/LivroForm.java
@@ -1,18 +1,18 @@
 package zcla71.baudoze.view.livro;
 
-import java.util.Collection;
+import java.util.List;
 
 import lombok.Data;
 
 @Data
 public class LivroForm {
     private String titulo;
-    private Collection<String> obras;
+    private List<String> obras;
     private String isbn13;
     private String isbn10;
     private Integer ano;
     private Integer paginas;
     private String edicao;
-    private Collection<String> editoras;
-    private Collection<String> etiquetas;
+    private List<String> editoras;
+    private List<String> etiquetas;
 }

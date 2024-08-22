450aaa6faa1e14547ec41dbc667002bfdf5a2b9e
zcla <zeclaudioaguiar@gmail.com>
2024-08-22 14:23:01 +0000

    import antigo

--- src/main/java/zcla71/tiddlywiki/Tiddler.java
+++ src/main/java/zcla71/tiddlywiki/Tiddler.java
@@ -7,7 +7,6 @@ import java.util.Map;
 import com.fasterxml.jackson.annotation.JsonAnyGetter;
 import com.fasterxml.jackson.annotation.JsonAnySetter;
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.Data;

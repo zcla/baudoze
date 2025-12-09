8853bf92e35b9e83edba1b5034caa4c8c069adca
zcla <zeclaudioaguiar@gmail.com>
2025-12-09 02:43:23 +0000

    Edit: indentação do select de tarefas

--- src/main/java/zcla71/baudoze/tarefa/view/TarefaEditarViewTarefaMae.java
+++ src/main/java/zcla71/baudoze/tarefa/view/TarefaEditarViewTarefaMae.java
@@ -6,14 +6,17 @@ import lombok.Data;
 public class TarefaEditarViewTarefaMae {
     private String value;
     private String text;
+    private Integer indent;
 
     public TarefaEditarViewTarefaMae() {
 		this.value = "";
         this.text = "";
+        this.indent = 0;
     }
 
 	public TarefaEditarViewTarefaMae(TarefaListaViewTarefa tarefa) {
 		this.value = tarefa.getId().toString();
         this.text = tarefa.getNome();
+        this.indent = tarefa.getIndent();
 	}
 }

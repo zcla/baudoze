679a320429aa95045ba7e7f7221a39a0980f305d
zcla <zeclaudioaguiar@gmail.com>
2026-02-21 03:16:48 +0000

    Mudança do tipo de campo

--- src/main/java/zcla71/baudoze/biblia/model/Versiculo.java
+++ src/main/java/zcla71/baudoze/biblia/model/Versiculo.java
@@ -28,6 +28,6 @@ public class Versiculo {
 	@Column(nullable = false)
 	private String numero;
 
-	@Column(nullable = false, length = 1024)
+	@Column(nullable = false, columnDefinition = "TEXT")
 	private String texto;
 }

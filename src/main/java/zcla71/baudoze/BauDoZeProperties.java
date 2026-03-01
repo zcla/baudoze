eab8163cad3d278c254539191a2f3a91e9b8bf18
zcla <zeclaudioaguiar@gmail.com>
2026-03-01 03:42:13 +0000

    Documentação

--- src/main/java/zcla71/baudoze/BauDoZeProperties.java
+++ src/main/java/zcla71/baudoze/BauDoZeProperties.java
@@ -8,6 +8,7 @@ import lombok.Data;
 
 @Data
 @ConfigurationProperties(prefix = "app")
+@Deprecated // TODO Já foi utilizado para pegar propriedades do application.yml; não uso mais, mas deixo para referência futura.
 public class BauDoZeProperties {
 	private Path storagePath = Path.of("./data");
 	private String storageProfile = "json";

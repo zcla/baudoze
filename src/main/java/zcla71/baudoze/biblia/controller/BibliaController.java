0d20b260ce460f562d5f7bc3419252a16c839c57
zcla <zeclaudioaguiar@gmail.com>
2026-03-13 02:40:41 +0000

    AuthUser como AuthenticationPrincipal também nos outros @Controller

--- src/main/java/zcla71/baudoze/biblia/controller/BibliaController.java
+++ src/main/java/zcla71/baudoze/biblia/controller/BibliaController.java
@@ -5,12 +5,12 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.lang.NonNull;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
 
+import zcla71.baudoze.auth_user.model.entity.AuthUser;
 import zcla71.baudoze.biblia.model.entity.Capitulo;
 import zcla71.baudoze.biblia.model.entity.Livro;
 import zcla71.baudoze.biblia.model.service.BibliaService;
@@ -27,9 +27,9 @@ public class BibliaController extends BauBaseController {
 	// Controller
 
 	@GetMapping("/biblia")
-	public ModelAndView biblia(@AuthenticationPrincipal OidcUser user) {
+	public ModelAndView biblia(@AuthenticationPrincipal AuthUser authUser) {
 		ModelAndView result = new ModelAndView("/biblia/biblias");
-		addAuthInfo(result, user);
+		addAuthInfo(result, authUser);
 		result.addObject("data", Map.of(
 			"biblias", bibliaViewService.listarBiblias()
 		));
@@ -37,9 +37,9 @@ public class BibliaController extends BauBaseController {
 	}
 
 	@GetMapping("/biblia/{idBiblia}")
-	public ModelAndView bibliaId(@AuthenticationPrincipal OidcUser user, @PathVariable @NonNull Long idBiblia) {
+	public ModelAndView bibliaId(@AuthenticationPrincipal AuthUser authUser, @PathVariable @NonNull Long idBiblia) {
 		ModelAndView result = new ModelAndView("/biblia/biblia");
-		addAuthInfo(result, user);
+		addAuthInfo(result, authUser);
 		result.addObject("data", Map.of(
 			"biblia", bibliaViewService.buscarBiblia(idBiblia),
 			"livros", bibliaViewService.listarLivros(idBiblia)
@@ -48,9 +48,9 @@ public class BibliaController extends BauBaseController {
 	}
 
 	@GetMapping("/biblia/livro/{idLivro}")
-	public ModelAndView livroId(@AuthenticationPrincipal OidcUser user, @PathVariable @NonNull Long idLivro) {
+	public ModelAndView livroId(@AuthenticationPrincipal AuthUser authUser, @PathVariable @NonNull Long idLivro) {
 		ModelAndView result = new ModelAndView("/biblia/livro");
-		addAuthInfo(result, user);
+		addAuthInfo(result, authUser);
 		Livro livro = bibliaService.buscaLivroPorId(idLivro);
 		Long idBiblia = livro.getBiblia().getId();
 		result.addObject("data", Map.of(
@@ -62,9 +62,9 @@ public class BibliaController extends BauBaseController {
 	}
 
 	@GetMapping("/biblia/livro/capitulo/{idCapitulo}")
-	public ModelAndView capituloId(@AuthenticationPrincipal OidcUser user, @PathVariable @NonNull Long idCapitulo) {
+	public ModelAndView capituloId(@AuthenticationPrincipal AuthUser authUser, @PathVariable @NonNull Long idCapitulo) {
 		ModelAndView result = new ModelAndView("/biblia/capitulo");
-		addAuthInfo(result, user);
+		addAuthInfo(result, authUser);
 		Capitulo capitulo = bibliaService.buscaCapituloPorId(idCapitulo);
 		Long idLivro = capitulo.getLivro().getId();
 		Long idBiblia = capitulo.getLivro().getBiblia().getId();
--- src/main/java/zcla71/baudoze/common/controller/BauController.java
+++ src/main/java/zcla71/baudoze/common/controller/BauController.java
@@ -1,19 +1,20 @@
 package zcla71.baudoze.common.controller;
 
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import zcla71.baudoze.auth_user.model.entity.AuthUser;
 
 @Controller
 public class BauController extends BauBaseController {
 	// Controller
 
 	@GetMapping("/")
-	public ModelAndView index(@AuthenticationPrincipal OidcUser user) {
+	public ModelAndView index(@AuthenticationPrincipal AuthUser authUser) {
 		ModelAndView result = new ModelAndView("/index");
-		addAuthInfo(result, user);
+		addAuthInfo(result, authUser);
 		return result;
 	}
 }

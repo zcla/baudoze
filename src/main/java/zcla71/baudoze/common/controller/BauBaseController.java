5f4860b62b001e5317064a1dac9082cb8fb9f4cd
zcla <zeclaudioaguiar@gmail.com>
2026-03-29 04:06:07 +0000

    BauAuthUser _authUser no ModelAndView

--- src/main/java/zcla71/baudoze/common/controller/BauBaseController.java
+++ src/main/java/zcla71/baudoze/common/controller/BauBaseController.java
@@ -3,15 +3,10 @@ package zcla71.baudoze.common.controller;
 import org.springframework.web.servlet.ModelAndView;
 
 import zcla71.baudoze.auth_user.model.entity.AuthUser;
+import zcla71.baudoze.common.model.BauAuthUser;
 
 public abstract class BauBaseController {
 	protected void addAuthInfo(ModelAndView mav, AuthUser authUser) {
-		// TODO Criar um objeto só "_authUser"?
-		mav.addObject("_authUserName", authUser.getNome());
-		if (authUser.getImagem() == null) {
-			mav.addObject("_authUserPicture", authUser.getUrlImagem());
-		} else {
-			mav.addObject("_authUserPicture", "/auth_user/" + authUser.getId() + "/imagem/");
-		}
+		mav.addObject("_authUser", new BauAuthUser(authUser.getNome(), authUser.getImagem() == null ? authUser.getUrlImagem() : "/auth_user/" + authUser.getId() + "/imagem/"));
 	}
 }
